c6b19cf1299ed3fa5e69dafda0d6bcd6d6e19a93
Claas <claas.moehlmann@outlook.com>
2023-03-20 18:37:00 +0100

    Kill my nerves

--- src/dim-ice-app.ts
+++ src/dim-ice-app.ts
@@ -1,10 +1,99 @@
 import { LitElement, html, css } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
 
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
+type AccessToken = string;
+type Scope = string;
+type UnixTimestampInSeconds = number;
+
+type AccessTokenResponse = {
+  access_token: AccessToken;
+  token_type: 'Bearer';
+  scope: Scope;
+  create_at: UnixTimestampInSeconds;
+};
+
+const logo = new URL('../../assets/logo.svg', import.meta.url).href;
+
+// I know, lol
+const clientSecret = '51Nwi4I83gxxgsqfPWZQLALHlIJgEjlIIh9o_Zdvh10';
+const clientId = 'NgtasltAXdbQgao8vU5H1pTLEBX1EGvuNThYYpUhoxA';
+
+const redirectUri = 'http://localhost:8001/redirect';
+
+//TODO make instance user-configurable
+const instance = 'mastodon.social';
+
+// Create urla
+const base = new URL(`https://${instance}/`);
+const authorizationUrl = new URL('/oauth/authorize', base);
+authorizationUrl.searchParams.set('client_id', clientId);
+authorizationUrl.searchParams.set('scope', 'read');
+// authorizationUrl.searchParams.set("redirect_uri", "urn:ietf:wg:oauth:2.0:oob");
+//TODO make redirect url current app url
+authorizationUrl.searchParams.set('redirect_uri', redirectUri);
+authorizationUrl.searchParams.set('response_type', 'code');
+
+async function exchangeForToken(
+  authorizationCode: string
+): Promise<AccessTokenResponse> {
+  const url = new URL('/oauth/token', base);
+  //TODO create type definition
+  const content = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    redirect_uri: redirectUri,
+    grant_type: 'authorization_code',
+    code: authorizationCode,
+    scope: 'read',
+  };
+
+  //TODO error handling
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(content),
+  });
+
+  return (await response.json()) as AccessTokenResponse;
+}
+
+async function getHomeTimeline(token: AccessToken) {
+  const url = new URL('/api/v1/timelines/home', base);
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.json();
+}
 
 @customElement('dim-ice-app')
 export class DimIceApp extends LitElement {
+  #code: string | undefined | null;
+
+  constructor() {
+    super();
+
+    if (location.pathname === '/redirect') {
+      // We are currently in the authorization flow when the app was opened with "/redirect"
+      // Assume we have the code in the query
+
+      const query = new URLSearchParams(location.search);
+      this.#code = query.get('code');
+
+      if (this.#code === null) {
+        console.warn('Expected code in query');
+      }
+
+      // Start token exchange
+      // obtainToken(this.#code).catch(console.error);
+    }
+  }
+
   @property({ type: String }) header = 'My app';
 
   static styles = css`
@@ -50,21 +139,36 @@ export class DimIceApp extends LitElement {
     }
   `;
 
+  async exchange() {
+    if (this.#code === null || this.#code === undefined) {
+      console.error('ooops');
+      return;
+    }
+
+    const response = await exchangeForToken(this.#code);
+
+    const timelines = await getHomeTimeline(response.access_token);
+
+    console.log(timelines);
+  }
+
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
+        <div class="logo"><img alt="dim ice logo" src=${logo} /></div>
         <h1>${this.header}</h1>
 
         <p>Edit <code>src/DimIceApp.ts</code> and save to reload.</p>
         <a
           class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
+          href="${authorizationUrl.toString()}"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Code examples
+          Authorize
         </a>
+
+        <button @click=${this.exchange}>Exchange</button>
       </main>
 
       <p class="app-footer">
